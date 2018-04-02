a3293dd84e296c31e034c4ff07af7883e8509b1e
Nathan Hinish <nathan.hinish@gmail.com>
2018-04-02 12:29:57 -0700

    apply security settings to node-red

--- roles/node-red/files/settings.js
+++ roles/node-red/files/settings.js
@@ -2,13 +2,21 @@ const settings = require(`${process.env[(process.platform == 'win32') ? 'USERPRO
 
 settings.disableEditor = true
 
-// settings.adminAuth = {
-//   type: 'credentials',
-//   users: [{
-//       username: 'admin',
-//       password: '$2a$08$NCCCL0iuGrg36B8u4cwgPevSbFnihVjuHM8OW5.aD9wz87cPJ4I7O',
-//       permissions: '*'
-//   }]
-// }
+settings.adminAuth = {
+  type: 'credentials',
+  users: [{
+      username: 'admin',
+      password: '$2a$08$NCCCL0iuGrg36B8u4cwgPevSbFnihVjuHM8OW5.aD9wz87cPJ4I7O',
+      permissions: '*'
+  }]
+}
+
+if (!settings.editorTheme) {
+  settings.editorTheme = {}
+}
+if (!settings.editorTheme.projects) {
+  settings.editorTheme.projects = {}
+}
+settings.editorTheme.projects.enabled = true
 
 module.exports = settings
